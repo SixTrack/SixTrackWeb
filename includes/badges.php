e6713d509eb74f6902601169fc36b40d378de208
Veronica K. B. Olsen <v.k.b.olsen@cern.ch>
2019-04-02 13:45:44 +0200

    Some minor tweaks.

--- includes/badges.php
+++ includes/badges.php
@@ -19,24 +19,24 @@
       break;
     }
     echo "<svg xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' width='88' height='20'>";
-    echo "  <linearGradient id='b' x2='0' y2='100%'>";
-    echo "    <stop offset='0' stop-color='#bbb' stop-opacity='.1'/>";
-    echo "    <stop offset='1' stop-opacity='.1'/>";
-    echo "  </linearGradient>";
-    echo "  <clipPath id='a'>";
-    echo "    <rect width='88' height='20' rx='3' fill='#fff'/>";
-    echo "  </clipPath>";
-    echo "  <g clip-path='url(#a)'>";
-    echo "    <path fill='#505050' d='M0 0h37v20H0z'/>";
-    echo "    <path fill='".$bgCol."' d='M37 0h51v20H37z'/>";
-    echo "    <path fill='url(#b)' d='M0 0h88v20H0z'/>";
-    echo "  </g>";
-    echo "  <g fill='#fff' text-anchor='left' font-family='DejaVu Sans,Verdana,Geneva,sans-serif' font-size='110'>";
-    echo "    <text x='55' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)'>".$cType."</text>";
-    echo "    <text x='55' y='140' transform='scale(.1)'>".$cType."</text>";
-    echo "    <text x='415' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)'>".$cStatus."</text>";
-    echo "    <text x='415' y='140' transform='scale(.1)'>".$cStatus."</text>";
-    echo "  </g>";
+    echo   "<linearGradient id='b' x2='0' y2='100%'>";
+    echo     "<stop offset='0' stop-color='#bbb' stop-opacity='.1'/>";
+    echo     "<stop offset='1' stop-opacity='.1'/>";
+    echo   "</linearGradient>";
+    echo   "<clipPath id='a'>";
+    echo     "<rect width='88' height='20' rx='3' fill='#fff'/>";
+    echo   "</clipPath>";
+    echo   "<g clip-path='url(#a)'>";
+    echo     "<path fill='#505050' d='M0 0h37v20H0z'/>";
+    echo     "<path fill='".$bgCol."' d='M37 0h51v20H37z'/>";
+    echo     "<path fill='url(#b)' d='M0 0h88v20H0z'/>";
+    echo   "</g>";
+    echo   "<g fill='#fff' text-anchor='left' font-family='DejaVu Sans,Verdana,Geneva,sans-serif' font-size='110'>";
+    echo     "<text x='55' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)'>".$cType."</text>";
+    echo     "<text x='55' y='140' transform='scale(.1)'>".$cType."</text>";
+    echo     "<text x='415' y='150' fill='#010101' fill-opacity='.3' transform='scale(.1)'>".$cStatus."</text>";
+    echo     "<text x='415' y='140' transform='scale(.1)'>".$cStatus."</text>";
+    echo   "</g>";
     echo "</svg>";
   }
 ?>
